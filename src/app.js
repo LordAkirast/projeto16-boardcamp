391f9f92458ea8e7eef9059b08e4c1f8d12a1320
LordAkirast <darkwarish@gmail.com>
2023-07-31 16:42:30 -0300

    Configurações de deploy

--- src/app.js
+++ src/app.js
@@ -505,4 +505,7 @@ app.delete("/rentals/:id", async (req,res) => {
 
 })
 
-app.listen(5000, () => console.log("Servidor ligado!"))
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
