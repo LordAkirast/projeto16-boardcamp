d440986af8e6466cb0ea70f42b02b0e9e0d7dea3
LordAkirast <darkwarish@gmail.com>
2023-07-30 22:05:07 -0300

    Fiz configurações nas rotas de alugar um jogo. Tava dando erro nas querys e também o algumas variaveis estavam declaradas dentro de ifs. mudei isso.

--- src/app.js
+++ src/app.js
@@ -219,30 +219,30 @@ app.put("/customers/:id", async (req, res) => {
 
     try {
         ///busca no banco onde o cpf e id é o mesmo. para alterar valor da mesma pessoa
-        const customer = await db.query('SELECT * FROM customers WHERE cpf = $1 and id = $2;', [cpf,id])
+        const customer = await db.query('SELECT * FROM customers WHERE cpf = $1 and id = $2;', [cpf, id])
         ///se encontrar, ele altera. 
         if (customer.rows.length > 0) {
             ///atualiza os dados que não seja cpf
             await db.query('UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;', [name, phone, cpf, birthday, id]);
             res.status(200).send("Cliente alterado!")
-            
+
         } else {
             //se não encontrar, faz mais uma busca, para ver se tem alguem com esse cpf no banco
             try {
                 ///se encontrar, ele não altera e dá erro
                 const customer = await db.query('SELECT * FROM customers WHERE cpf = $1;', [cpf])
                 if (customer.rows.length > 0) {
-                    res.status(409).send(`O CPF ${cpf} não pode ser o de outro cliente!`)  
-                    
+                    res.status(409).send(`O CPF ${cpf} não pode ser o de outro cliente!`)
+
                 } else {
                     ///se não encontrar, ele altera
                     await db.query('UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5;', [name, phone, cpf, birthday, id]);
                     res.status(200).send("Cliente alterado!")
                 }
             } catch (error) {
-                
+
             }
-            res.status(409).send(`O CPF ${cpf} não pode ser o de outro cliente!`)  
+            res.status(409).send(`O CPF ${cpf} não pode ser o de outro cliente!`)
         }
     } catch (err) {
         res.status(500).send(err.message)
@@ -267,7 +267,7 @@ app.post("/rentals", async (req, res) => {
 
     let { customerId, gameId, daysRented } = req.body
 
-    
+
 
     try {
         const jogos = await db.query(`SELECT * FROM games where id = $1`, [gameId])
@@ -275,11 +275,9 @@ app.post("/rentals", async (req, res) => {
             res.status(404).send("Jogo não encontrado!")
         } else {
             let pricePerDay = jogos.rows[0].pricePerDay
-            console.log('jogo encontrado com sucesso')
+            console.log('jogo encontrado com sucesso - 1')
 
 
-
-            daysRented = Date.now()
             let originalPrice = daysRented * pricePerDay
 
 
@@ -288,12 +286,14 @@ app.post("/rentals", async (req, res) => {
                 gameId: 1,
                 daysRented: 3
             }
+
             ///parte de data
             const dataAtual = dayjs();
-            const rentDate = dataAtual.format('YYYY-MM-DD')
+            let rentDate = dataAtual.format('YYYY-MM-DD')
             ///
-            let returnDate = null
-            let delayFee = null
+            let returnDate = null;
+            let delayFee = null;
+
 
 
             const validation = createRent.validate({ customerId, gameId, daysRented }, { abortEarly: "False" })
@@ -310,42 +310,61 @@ app.post("/rentals", async (req, res) => {
 
             try {
                 const jogos = await db.query('SELECT rentals.*, games.* FROM rentals JOIN games ON rentals."gameId" = games.id WHERE rentals."gameId" = $1 AND games.id = $2;', [gameId, gameId]);
-
+                console.log(jogos.rows.length)
                 if (jogos.rows.length > 0) {
                     ///se for maior que zero. significa que o jogo ja foi alugado. portanto, vai diminuir stocktotal em 1
-                  const jogo = jogos.rows[0];
-                  if (jogo.stockTotal === 0) {
-                    return res.status(400).send("Erro: Não há jogo em estoque.");
-                  } else {
-                    await db.query('INSERT INTO rentals (customerId, gameId. rentDate, daysRented, returnDate, originalPrice, delayFee) values($1, $2, $3, $4, $5, $6, $7)'[customerId, gameId. rentDate, daysRented, returnDate, originalPrice, delayFee])
-                    await db.query('UPDATE games SET "stockTotal" = "stockTotal" - 1 WHERE id = $1', [gameId]);
-                    return res.status(201).send("Jogo alugado!")
-                  }
+                    const jogo = jogos.rows[0];
+                    console.log('fase 2')
+                    console.log(jogo.stockTotal)
+                    if (jogo.stockTotal === 0) {
+                        console.log('fase 3')
+                        return res.status(400).send("Erro: Não há jogo em estoque.");
+                    } else {
+                        await db.query(
+                            'INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)',
+                            [customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee]
+                        );
+                        await db.query('UPDATE games SET "stockTotal" = "stockTotal" - 1 WHERE id = $1', [gameId]);
+                        return res.status(201).send(`Jogo: ${jogo.name} alugado com sucesso! Restam: ${jogo.stockTotal} unidades!`)
+                    }
                 } else {
+                    console.log('caiu no else')
                     ///se cair aqui, preciso verificar primeiro se o jogo existe, se não existir dá erro, caso contrário, segue o fluxo e diminui
                     ///stocktotal em 1. a ideia é que aqui é se for o primeiro aluguel do jogo
-                   const gameVerify = await db.query('SELECT * from games where id = $1', [gameId])
-                   if (gameVerify.rows <= 0 ) {
-                    return res.status(404).send("Jogo não encontrado.");
-                   }
-                    const jogo = jogos.rows[0];
-                  if (jogo.stockTotal === 0) {
-                    return res.status(400).send("Erro: Não há jogo em estoque.");
-                  } else {
-                    await db.query('INSERT INTO rentals (customerId, gameId. rentDate, daysRented, returnDate, originalPrice, delayFee) values($1, $2, $3, $4, $5, $6, $7)'[customerId, gameId. rentDate, daysRented, returnDate, originalPrice, delayFee])
-                    await db.query('UPDATE games SET "stockTotal" = "stockTotal" - 1 WHERE id = $1', [gameId]);
-                    return res.status(201).send(`Jogo: ${jogo.name} alugado com sucesso! Restam: ${jogo.stockTotal} unidades!`)
-                  }
-                    
-                 
+                    const gameVerify = await db.query('SELECT * from games where id = $1', [gameId])
+                    console.log('gameID:', gameId)
+                    if (gameVerify.rows <= 0) {
+                        return res.status(404).send("Jogo não encontrado.");
+                    }
+                    const jogo = gameVerify.rows[0];
+                    console.log('jogo' + gameVerify.rows[0])
+                    console.log('gameID 1:', gameId)
+                    if (jogo.stockTotal === 0) {
+                        return res.status(400).send("Erro: Não há jogo em estoque.");
+                    } else {
+                        console.log('entrou no segundo else')
+                        
+        
+                        console.log('itens: ' + customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee)
+                        await db.query(
+                            'INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)',
+                            [customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee]
+                        );
+                        await db.query('UPDATE games SET "stockTotal" = "stockTotal" - 1 WHERE id = $1', [gameId]);
+                        return res.status(201).send(`Jogo: ${jogo.name} alugado com sucesso! Restam: ${jogo.stockTotal} unidades!`)
+                    }
+
+
                 }
-                
+
             } catch (err) {
+                console.log('ERRRU22')
                 res.status(500).send(err.message)
 
             }
         }
     } catch (err) {
+        console.log('ERRRU')
         res.status(500).send(err.message)
 
     }
